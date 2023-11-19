c97ccd526a634b2efa82f2beeb7e56d8cd23a632
Mårten Pettersson <mtnptrsn@gmail.com>
2023-11-19 10:37:48 +0100

    install more bins with brew

--- install/install-binaries.sh
+++ install/install-binaries.sh
@@ -1,2 +1,14 @@
 brew install exa
 brew install alacritty
+brew install nvim
+brew install discord
+brew install google-chrome
+brew install --cask 1password
+brew install docker
+brew install docker-compose
+
+brew install mas
+# Flow (Pomodoro Timer)
+mas install 1423210932
+
+brew cleanup
