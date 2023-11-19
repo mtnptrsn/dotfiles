67c9ab98b2672bf7f5dfbfaa5ca710a8355a94c2
Mårten Pettersson <mtnptrsn@gmail.com>
2023-11-19 10:45:53 +0100

    remove uncessary bins and use alias instead

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,14 +3,8 @@ alias pip="pip3"
 alias python="python3"
 alias rgf="rg --files | rg"
 alias ls="exa"
+alias docker-ps-watch="watch 'docker ps -q | xargs -L 1 docker logs'"
+alias docker-logs-watch="watch 'docker ps --format \"table {{.ID}}\t{{.Names}}\t{{.Ports}}\"'"
 alias home="cd $HOME"
-
-neorg () {
-  cd $HOME/Neorg/
-  nvim .
-}
-
-dotfiles () {
-  cd $HOME/.dotfiles/
-  nvim .
-}
+alias neorg="cd $HOME/Neorg && nvim ."
+alias dotfiles="cd $HOME/.dotfiles && nvim ."
