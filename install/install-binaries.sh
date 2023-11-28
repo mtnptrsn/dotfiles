d7a593c9a1da0d426f17f81759b38d63ca6af389
Mårten Pettersson <mtnptrsn@gmail.com>
2023-11-28 13:13:00 +0100

    add astronvim to install-binaries

--- install/install-binaries.sh
+++ install/install-binaries.sh
@@ -1,6 +1,9 @@
 brew install exa
 brew install alacritty
-brew install nvim
+
+brew install neovim
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
 brew install discord
 brew install google-chrome
 brew install --cask 1password
@@ -9,5 +12,6 @@ brew install --cask docker
 brew install mas
 # Flow (Pomodoro Timer)
 mas install 1423210932
+
 
 brew cleanup
