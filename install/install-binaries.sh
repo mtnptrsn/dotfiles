c23999082de7f0ee0a6cc6f7ec23a3bdb2951ccc
marten.pettersson <marten.pettersson@modelon.com>
2023-12-05 11:39:29 +0100

    add to install-binaries

--- install/install-binaries.sh
+++ install/install-binaries.sh
@@ -1,8 +1,14 @@
 brew install exa
 brew install alacritty
 
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 brew install neovim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code-nerd-font
 
 brew install discord
 brew install google-chrome
