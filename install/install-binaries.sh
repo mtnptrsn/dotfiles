2c671f4ad842b2b439425cf181de53c342d1c5c1
Mårten Pettersson <mtnptrsn@gmail.com>
2023-12-09 21:11:52 +0100

    add comments to install-binaries

--- install/install-binaries.sh
+++ install/install-binaries.sh
@@ -1,23 +1,29 @@
-brew install exa
-brew install alacritty
-
+# Tmux
 brew install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Neovim
 brew install neovim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
+# Fonts
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code-nerd-font
 
-brew install discord
-brew install google-chrome
+# Apps
+brew install --cask discord
+brew install --cask google-chrome
 brew install --cask 1password
 brew install --cask docker
 
+# Misc
+brew install exa
+brew install alacritty
+
+# App Store
 brew install mas
+
 # Flow (Pomodoro Timer)
 mas install 1423210932
-
 
 brew cleanup
