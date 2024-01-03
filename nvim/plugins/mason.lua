7f59ba4995726cec1d8e5a889288f5975bb11d62
Mårten Pettersson <mtnptrsn@gmail.com>
2024-01-03 16:01:49 +0100

    use none-ls instead of null-ls

--- nvim/plugins/mason.lua
+++ nvim/plugins/mason.lua
@@ -15,7 +15,7 @@ return {
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
-    "jay-babu/mason-null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
--- nvim/plugins/null-ls.lua
+++ nvim/plugins/null-ls.lua
@@ -1,5 +1,5 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  "jay-babu/mason-null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     -- local null_ls = require "null-ls"
