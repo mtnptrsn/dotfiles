d1baf1e769554db89d86f43ecee9627bc05fdc6f
Mårten Pettersson <mtnptrsn@gmail.com>
2024-10-04 00:00:42 +0200

    open nvim with no-neck-pain by default

--- nvim/lua/plugins/no-neck-pain.lua
+++ nvim/lua/plugins/no-neck-pain.lua
@@ -1,6 +1,14 @@
 return {
 	"shortcuts/no-neck-pain.nvim",
 	config = function()
+		-- Create a key mapping to toggle No Neck Pain mode
 		vim.api.nvim_set_keymap("n", "<leader>un", ":NoNeckPain<CR>", { noremap = true, silent = true })
+
+		-- Enable No Neck Pain mode automatically when Neovim starts
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				vim.cmd("NoNeckPain")
+			end,
+		})
 	end,
 }
