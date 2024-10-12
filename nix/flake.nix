169a5ee5dbfe383fa7baf37e7a108fb11bd098c4
Mårten Pettersson <mtnptrsn@gmail.com>
2024-10-12 10:44:09 +0200

    format and add fira code

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,19 +1,15 @@
 {
   description = "Example Darwin system flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
-
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
     let
       configuration = { pkgs, config, ... }: {
-
         nixpkgs.config.allowUnfree = true;
-
         environment.systemPackages =
           [
             pkgs.neovim
@@ -27,8 +23,11 @@
             pkgs.rustup
             pkgs.ripgrep
             pkgs.fzf
+            pkgs.fira-code
           ];
-
+        fonts.packages = [
+          (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+        ];
         homebrew = {
           enable = true;
           casks = [
@@ -40,28 +39,12 @@
           onActivation.autoUpdate = true;
           onActivation.upgrade = true;
         };
-
-        # Auto upgrade nix package and the daemon service.
         services.nix-daemon.enable = true;
-        # nix.package = pkgs.nix;
-
-        # Necessary for using flakes on this system.
         nix.settings.experimental-features = "nix-command flakes";
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
         programs.zsh.enable = true; # default shell on catalina
-        # programs.fish.enable = true;
-
-        # Set Git commit hash for darwin-version.
         system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
         system.stateVersion = 5;
-
-        # The platform the configuration will be used on.
         nixpkgs.hostPlatform = "aarch64-darwin";
-
         system.activationScripts.applications.text =
           let
             env = pkgs.buildEnv {
@@ -90,28 +73,19 @@
       };
     in
     {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#
       darwinConfigurations."mtnptrsn" = nix-darwin.lib.darwinSystem {
         modules = [
           configuration
           nix-homebrew.darwinModules.nix-homebrew
           {
             nix-homebrew = {
-              # Install Homebrew under the default prefix
               enable = true;
-
-              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
               enableRosetta = true;
-
-              # User owning the Homebrew prefix
               user = "mtnptrsn";
             };
           }
         ];
       };
-
-      # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations."mtnptrsn".pkgs;
     };
 }
